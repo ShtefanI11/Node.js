65ac4c36c18ede9d89d7ebc06066d18d9de432ae
Igor Shtefan <shtefan.i333@gmail.com>
2023-11-21 22:21:15 +0200

    Task:3-Creating and connecting controllers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import v1Router from './v1/routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
+app.use('/api/v1', v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
